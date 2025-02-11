ec3d3ad48f11e1f768d70afb03ea0f5b2e822d48
Lonpsy <oolalonpe@gmail.com>
2025-02-11 01:31:46 +0100

    updated the city with basic javacsript funtionalities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("hello");
+function updateCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#enter-city");
+  console.log(`${city.value}`);
+  let updatedCity = document.querySelector("#weather-app-city");
+  updatedCity.innerHTML = city.value;
+}
+
+let formInput = document.querySelector("#weather-form");
+formInput.addEventListener("submit", updateCity);
