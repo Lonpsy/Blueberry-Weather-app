75015fae55f300f866b263baf59cffb72dcf99fa
Lonpsy <oolalonpe@gmail.com>
2025-02-12 05:08:38 +0100

    updated the temperature Api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-function cityInputDetails(updatedCity) {
-  let key = "da7a1b3d460dbtbf7b304o1bb99604f1";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${updatedCity}&key={key}`;
+function updateTemperature(response) {
+  let Temperature = document.querySelector("#tempEntered");
+  Temperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function cityInputDetails(City) {
+  let key = "0dbtbf7b304da7a1b3d46o1bb99604f1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${City}&key=${key}&unit=metric`;
+  axios.get(apiUrl).then(updateTemperature);
 }
 
 function updateCity(event) {
@@ -8,7 +14,7 @@ function updateCity(event) {
   let city = document.querySelector("#enter-city");
   let updatedCity = document.querySelector("#weather-app-city");
   updatedCity.innerHTML = city.value;
-  cityInputDetails(city.input);
+  cityInputDetails(city.value);
 }
 
 let formInput = document.querySelector("#weather-form");
