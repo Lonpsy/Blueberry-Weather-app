23f742d6258cc70877fb12084d3324152bec48f0
Lonpsy <oolalonpe@gmail.com>
2025-02-12 04:31:31 +0100

    introduced axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+function cityInputDetails(updatedCity) {
+  let key = "da7a1b3d460dbtbf7b304o1bb99604f1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${updatedCity}&key={key}`;
+}
+
 function updateCity(event) {
   event.preventDefault();
   let city = document.querySelector("#enter-city");
-  console.log(`${city.value}`);
   let updatedCity = document.querySelector("#weather-app-city");
   updatedCity.innerHTML = city.value;
+  cityInputDetails(city.input);
 }
 
 let formInput = document.querySelector("#weather-form");
